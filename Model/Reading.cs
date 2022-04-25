8ef06d4a23fbdd0640a8a18d38e73c291e7b29f7
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2022-04-25 13:33:54 +0200

    SQLiteCommand

--- Model/Reading.cs
+++ Model/Reading.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Data;
 
 namespace database
 {
-    public class Reading : IReading
+    public class Reading 
     {
+
+        private readonly IDatabaseProvider provider;
+
+        private IDbConnection connection;
+
+        public Reading(IDatabaseProvider provider)
+        {
+            this.provider = provider;
+
+        }
+
+        private void CreateDatabaseTables()
+        {
+            var cmd = new SQLiteCommand($"CREATE TABLE IF NOT EXISTS characters (Id INTEGER PRIMARY KEY, Name VARCHAR(50), Experience INTEGER);", (SQLiteConnection)connection);
+            cmd.ExecuteNonQuery();
+        }
+
+
 
         public event Action <int> OnActualUpdate;
         private int actluel;
